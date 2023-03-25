2290f67df7634904422dae958a0b9cb25a94c0ab
Symplice <intwarisymplice@gmail.com>
2023-03-25 16:44:13 +0200

    build:remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Login from './Components/Login';
 import reportWebVitals from './reportWebVitals';
 import Dashboard from './Components/Dashboard';
-import Dropdown from './Components/Dropdown';
-import Sidebar from './Components/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Login /> */}
     <Dashboard />
-    {/* <Dropdown /> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
